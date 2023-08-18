9f4424e444863524c3597bcd2865b7e6cbddba4e
Kriti <kritig.0607@gmail.com>
2023-08-18 23:38:02 +0530

    description

---
+++ lib/login_page.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String email = "";
+  String pass = "";
+
+  bool isHiddenPassword = true;
+
+  Widget buildForgotPass(){
+    return Container(
+      alignment: Alignment.centerRight,
+      child :   MaterialButton(
+        onPressed: () {},
+        padding: EdgeInsets.only(right: 0),
+        child: Text(
+          'Forgot Password?',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSignUpBtn(){
+    return GestureDetector(
+      onTap: () {Navigator.pushNamed(context, "register_company");},
+      child: RichText(
+        text: TextSpan(
+            children: [
+              TextSpan(
+                  text: 'Don\'t have an Account? ',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500
+                  )
+              ),
+              TextSpan(
+                  text: 'Sign Up',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                  )
+              )
+            ]
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 50,),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 50,
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text("NoTA", style:TextStyle(color: Colors.white, fontSize:40,fontWeight: FontWeight.bold,fontFamily:'Raleway'),),
+                ],
+              ),
+            ),
+            SizedBox(height: 10,),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 60.0,
+                  horizontal: 30.0,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10),)
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 20,),
+                        Text("Welcome Back!", style:TextStyle(color: Colors.black, fontSize: 30,fontFamily:'Raleway'),),
+
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                SizedBox(height: 20,),
+                                Text(
+                                  'Email or username',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[100],
+                                    borderRadius: BorderRadius.circular(10),
+                                    //       boxShadow: [BoxShadow(
+                                    //       color: Color.fromRGBO(22, 224, 32, 0.298),
+                                    //       blurRadius: 20,
+                                    //        offset: Offset(0, 10)
+                                    //  ),]
+                                  ),
+                                  child: TextField(
+                                    onChanged: (value) {
+                                      email = value;
+                                    },
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        contentPadding: EdgeInsets.only(top: 14),
+                                        prefixIcon: Icon(
+                                          Icons.email,
+                                          color: Colors.teal,
+                                        ),
+                                        hintText: 'Email or username',
+                                        hintStyle: TextStyle(
+                                          color: Colors.black38,
+                                        )
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 20,),
+                                Text(
+                                  'Password',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[100],
+                                    borderRadius: BorderRadius.circular(10),
+                                    //       boxShadow: [BoxShadow(
+                                    //       color: Color.fromRGBO(22, 224, 32, 0.298),
+                                    //       blurRadius: 20,
+                                    //        offset: Offset(0, 10)
+                                    //  ),]
+                                  ),
+                                  child: TextField(
+                                    onChanged: (value) {
+                                      email = value;
+                                    },
+                                    obscureText: isHiddenPassword,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        contentPadding: EdgeInsets.only(top: 14),
+                                        prefixIcon: Icon(
+                                          Icons.lock,
+                                          color: Colors.teal,
+                                        ),
+                                        suffixIcon: InkWell(
+                                          onTap: _togglePasswordView,
+                                          child: Icon(
+                                            isHiddenPassword ? Icons.visibility :Icons.visibility_off,
+                                          ),
+                                        ),
+                                        hintText: 'Password',
+                                        hintStyle: TextStyle(
+                                          color: Colors.black38,
+                                        )
+                                    ),
+                                  ),
+                                ),
+                              ]
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        buildForgotPass(),
+                        SizedBox(height: 30,),
+                        Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          child: Center(
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(fixedSize: const Size(150, 70),shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50.0),),backgroundColor: Colors.teal, foregroundColor: Colors.white),
+                              child: Text(
+                                "Login",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 40,),
+                        buildSignUpBtn(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  void _togglePasswordView(){
+    if(isHiddenPassword == true){
+      isHiddenPassword = false;
+    }else{
+      isHiddenPassword = true;
+    }
+    setState(() {});
+  }
+}
--- lib/page_details_page.dart
+++ lib/page_details_page.dart
@@ -1,7 +1,13 @@
 import 'dart:ui';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'note_page.dart';
+import 'text_to_image_page.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PageDetailsPage extends StatefulWidget {
   final NotePage page;
@@ -14,6 +20,18 @@ class PageDetailsPage extends StatefulWidget {
 
 class _PageDetailsPageState extends State<PageDetailsPage> {
   TextEditingController _textEditingController = TextEditingController();
+  bool _isDrawing = false;
+  List<List<Offset>> _drawingPaths = [];
+
+  Color _selectedColor = Colors.black;
+  double _selectedStrokeWidth = 5.0;
+  GlobalKey _drawingKey = GlobalKey();
+
+  List<DrawnLine> _lines = [];
+  DrawnLine? _currentLine;
+
+  Color _textContainerColor = Colors.white;
+  String? _selectedImagePath;
 
   @override
   void initState() {
@@ -58,35 +76,7 @@ class _PageDetailsPageState extends State<PageDetailsPage> {
             ),
             Padding(
               padding: EdgeInsets.only(top: 100),
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: Card(
-                        elevation: 4,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16)),
-                        child: Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: TextField(
-                            controller: _textEditingController,
-                            maxLines: null,
-                            onChanged: (value) {
-                              setState(() {
-                                widget.page.content = value;
-                              });
-                            },
-                            decoration: InputDecoration.collapsed(
-                                hintText: 'Type your notes here...'),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              child: _buildContent(),
             ),
           ],
         ),
@@ -104,31 +94,100 @@ class _PageDetailsPageState extends State<PageDetailsPage> {
               IconButton(
                 icon: Icon(Icons.touch_app),
                 onPressed: () {
-                  // Gesture detector
+                  setState(() {
+                    _isDrawing = !_isDrawing;
+                    if (!_isDrawing) {
+                      _lines.add(DrawnLine(
+                        points: [],
+                        color: _selectedColor,
+                        width: _selectedStrokeWidth,
+                      )); // Finish the current line when turning off drawing
+                    }
+                  });
                 },
               ),
               IconButton(
                 icon: Icon(Icons.image),
                 onPressed: () {
-                  // Image picker
+                  pickImageFromGallery();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.text_fields),
-                onPressed: () {
-                  // Text editor
-                },
+                onPressed: () {},
               ),
               IconButton(
                 icon: Icon(Icons.color_lens),
                 onPressed: () {
-                  // Backgroud change
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Select Background Color'),
+                        content: SingleChildScrollView(
+                          child: ColorPicker(
+                            pickerColor: _textContainerColor,
+                            onColorChanged: (color) {
+                              setState(() {
+                                _textContainerColor = color;
+                              });
+                            },
+                          ),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('OK'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 },
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildAiButtonWithMenu() {
+    return SpeedDial(
+      backgroundColor: Colors.teal,
+      animatedIcon: AnimatedIcons.menu_close,
+      animatedIconTheme: IconThemeData(size: 22.0),
+      curve: Curves.bounceIn,
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.image_search, color: Colors.white),
+          backgroundColor: Colors.orangeAccent,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TextToImagePage()),
+            );
+          },
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.co_present, color: Colors.white),
+          backgroundColor: Colors.blue,
+          // label: 'Contact',
+          onTap: () {
+            // Implement contact functionality here
+          },
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.text_snippet_outlined, color: Colors.white),
+          backgroundColor: Colors.pinkAccent,
+          // label: 'Image to Text',
+          onTap: () {
+            // Implement image to text extraction functionality here
+          },
+        ),
+      ],
     );
   }
 
@@ -158,46 +217,307 @@ class _PageDetailsPageState extends State<PageDetailsPage> {
     );
   }
 
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onPanStart: _onPanStart,
+                  onPanUpdate: _onPanUpdate,
+                  onPanEnd: _onPanEnd,
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    color: _textContainerColor,
+                    child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: _isDrawing
+                          ? CustomPaint(
+                        painter: DrawingPainter(
+                          _lines,
+                        ),
+                      )
+                          : TextField(
+                        controller: _textEditingController,
+                        maxLines: null,
+                        onChanged: (value) {
+                          setState(() {
+                            widget.page.content = value;
+                          });
+                        },
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type your notes here...',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          if (_isDrawing) // Show buttons when drawing is active
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0), // Padding between buttons
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ..._buildColorButtons(),
+                    ..._buildStrokeButtons(),
+                    IconButton(
+                      icon: Icon(Icons.refresh),
+                      onPressed: () {
+                        setState(() {
+                          _lines.clear();
+                        });
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.save),
+                      onPressed: () {
+                        _saveDrawing();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          if (_colorLensSelected) // Show color options popup when color_lens is selected
+            Positioned(
+              bottom: 50, // Adjust the position as needed
+              left: 16,
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: _buildColorOptions(),
+                ),
+              ),
+            ),
+          if (_selectedImagePath != null) // Display selected image
+            Positioned(
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Image.file(
+                File(_selectedImagePath!),
+                fit: BoxFit.cover,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildColorButtons() {
+    return [
+      _buildColorButton(Colors.black),
+      _buildColorButton(Colors.red),
+      _buildColorButton(Colors.green),
+      _buildColorButton(Colors.blue),
+      _buildColorButton(Colors.yellow),
+      //_buildColorButton(Colors.purple),
+     // _buildColorButton(Colors.orange),
+    ];
+  }
+
+  Widget _buildColorButton(Color color) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedColor = color;
+        });
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          border: _selectedColor == color
+              ? Border.all(color: Colors.black, width: 2)
+              : null,
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildStrokeButtons() {
+    return [
+      _buildStrokeButton(2.0),
+      _buildStrokeButton(5.0),
+      _buildStrokeButton(10.0),
+    ];
+  }
+
+  Widget _buildStrokeButton(double strokeWidth) {
+    return GestureDetector(
+      onTap: () {
+        print("Stroke width tapped: $strokeWidth");
+        setState(() {
+          _selectedStrokeWidth = strokeWidth;
+        });
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: _selectedStrokeWidth == strokeWidth
+                ? Colors.black
+                : Colors.grey,
+            width: 2,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            width: strokeWidth,
+            height: 5,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+
+  bool _colorLensSelected = false; // To track whether the color lens button is selected
+
+  List<Widget> _buildColorOptions() {
+    return [
+      _buildColorOption(Colors.red),
+      _buildColorOption(Colors.green),
+      _buildColorOption(Colors.blue),
+      // Add more color options here
+    ];
+  }
+
+  Widget _buildColorOption(Color color) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _textContainerColor = color;
+        });
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        margin: EdgeInsets.symmetric(horizontal: 4),
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+
+  void _onPanStart(DragStartDetails details) {
+    final point = details.localPosition;
+    setState(() {
+      _currentLine = DrawnLine(
+        points: [point],
+        color: _selectedColor,
+        width: _selectedStrokeWidth,
+      );
+    });
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    final point = details.localPosition;
+    final List<Offset> path = List.from(_currentLine!.points)..add(point);
+    setState(() {
+      _currentLine = DrawnLine(
+        points: path,
+        color: _selectedColor,
+        width: _selectedStrokeWidth,
+      );
+    });
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_currentLine != null) {
+      setState(() {
+        _lines.add(_currentLine!);
+        _currentLine = null;
+      });
+    }
+  }
+
+  Future<void> _saveDrawing() async {
+    print("Save button pressed");
+    try {
+      final boundary = _drawingKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      print("Capturing image...");
+      final image = await boundary.toImage();
+      print("Image captured");
+      final byteData = await image.toByteData(format: ImageByteFormat.png);
+
+      if (byteData != null) {
+        final pngBytes = byteData.buffer.asUint8List();
+        final saved = await ImageGallerySaver.saveImage(
+          pngBytes,
+          quality: 100,
+          name: DateTime.now().toIso8601String() + ".png",
+          isReturnImagePathOfIOS: true,
+        );
+        print(saved);
+
+        setState(() {
+          _drawingPaths.clear(); // Clear the drawing paths after saving
+        });
+      } else {
+        print("ByteData is null.");
+      }
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  Future<void> pickImageFromGallery() async {
+    try {
+      final imagePicker = ImagePicker();
+      final pickedImage = await imagePicker.pickImage(source: ImageSource.gallery);
+
+      if (pickedImage != null) {
+        setState(() {
+          _selectedImagePath = pickedImage.path; // Store the selected image path
+        });
+      } else {
+        // User cancelled the image selection
+      }
+    } catch (e) {
+      print("Error picking image: $e");
+    }
+  }
+
   void _savePage() {
     // Implement the auto-save functionality here
     // For example, you can save the content to the database or storage
     widget.page.content = _textEditingController.text;
   }
 
-  Widget _buildAiButtonWithMenu() {
-    return SpeedDial(
-      backgroundColor: Colors.teal,
-      animatedIcon: AnimatedIcons.menu_close,
-      animatedIconTheme: IconThemeData(size: 22.0),
-      curve: Curves.bounceIn,
-      children: [
-        SpeedDialChild(
-          child: Icon(Icons.image_search, color: Colors.white),
-          backgroundColor: Colors.orangeAccent,
-          // label: 'Text to Image',
-          onTap: () {
-            // Implement icon text to image functionality here
-          },
-        ),
-        SpeedDialChild(
-          child: Icon(Icons.co_present, color: Colors.white),
-          backgroundColor: Colors.blue,
-          // label: 'Contact',
-          onTap: () {
-            // Implement contact functionality here
-          },
-        ),
-        SpeedDialChild(
-          child: Icon(Icons.text_snippet_outlined, color: Colors.white),
-          backgroundColor: Colors.pinkAccent,
-          // label: 'Image to Text',
-          onTap: () {
-            // Implement image to text extraction functionality here
-          },
-        ),
-      ],
-    );
-  }
  }
 
 class BackgroundCirclePainter extends CustomPainter {
@@ -237,3 +557,41 @@ class BackgroundCircleClipper extends CustomClipper<Path> {
     return false;
   }
 }
+
+class DrawingPainter extends CustomPainter {
+  final List<DrawnLine> lines;
+
+  DrawingPainter(this.lines);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (final line in lines) {
+      final paint = Paint()
+        ..color = line.color
+        ..strokeCap = StrokeCap.round
+        ..strokeWidth = line.width;
+
+      final path = Path()..addPolygon(line.points, false);
+
+      canvas.drawPath(path, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class DrawnLine {
+  final List<Offset> points;
+  final Color color;
+  final double width;
+
+  DrawnLine({
+    required this.points,
+    required this.color,
+    required this.width,
+  });
+}
+
